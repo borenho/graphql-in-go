5a103cbbb8c1b908f428442d4887e4bdb03b9f9a
Kaka Ruto <borecollins@gmail.com>
2019-06-04 23:20:38 +0300

    Add object types

--- main.go
+++ main.go
@@ -8,6 +8,76 @@ import (
 	"github.com/graphql-go/graphql"
 )
 
+// Types
+type Tutorial struct {
+	Title    string
+	Author   Author
+	Comments []Comment
+}
+
+type Author struct {
+	Name      string
+	Tutorials []int
+}
+
+type Comment struct {
+	Body string
+}
+
+// Function to return an array of tutorials
+func populate() []Tutorial {
+	author := &Author{Name: "Kevin Terah", Tutorials: []int{1}}
+	tutorial := Tutorial{
+		ID:     1,
+		Title:  "Environmental Hygiene",
+		Author: *author,
+		Comments: []Comment{
+			Comment{Body: "Climate change is ..."},
+		},
+	}
+
+	var tutorials []Tutorial
+	tutorials = append(tutorials, tutorial)
+
+	return tutorials
+}
+
+// Objects
+var commentType = graphql.NewObject(
+	graphql.ObjectConfig{
+		// Define the name and fields of the object
+		Name: "Comment",
+		Fields: graphql.Fields{
+			"body": &graphql.Field{Type: graphql.String},
+		},
+	},
+)
+
+var authorType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Author",
+		Fields: graphql.Fields{
+			"Name": &graphql.Field{Type: graphql.String},
+			"Tutorials": &graphql.Field{
+				// Use NewList to hold an array of integers
+				Type: graphql.NewList(graphql.Int),
+			},
+		},
+	},
+)
+
+var tutorialType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Tutorial",
+		Fields: graphql.Fields{
+			"id":       &graphql.Field{Type: graphql.Int},
+			"title":    &graphql.Field{Type: graphql.String},
+			"author":   &graphql.Field{Type: authorType},
+			"comments": &graphql.Field{Type: graphql.NewList(commentType)},
+		},
+	},
+)
+
 func main() {
 	// Schema
 	fields := graphql.Fields{
